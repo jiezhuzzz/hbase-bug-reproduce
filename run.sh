56fa789ba17aec9478c87898daa2ffde6c91b280
Jie Zhu <chore.08-protist@icloud.com>
2023-03-09 19:45:02 -0600

    Update run.sh
    Convert data to csv

--- run.sh
+++ run.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 
+echo "calls, log_size" >> output.csv
 
 for n in {10..100..10};
 do
   MAX_ITER="$n" mvn test -Dtest=TestOutputSinkWriter &> /dev/null
   sleep 1
-  stat -c%s hbase-server/target/surefire-reports/org.apache.hadoop.hbase.wal.TestOutputSinkWriter-output.txt >> output.txt
+  log_size=$(stat -c%s hbase-server/target/surefire-reports/org.apache.hadoop.hbase.wal.TestOutputSinkWriter-output.txt)
   sleep 1
+  echo "$n,$log_size" >> output.csv
 done
